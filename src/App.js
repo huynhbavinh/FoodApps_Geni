575ad6dbbea0e139a8b3152991a5e2c0db7387ac
huỳnh bá vinh <55732546+huynhbavinh@users.noreply.github.com>
2023-09-04 13:41:14 +0700

    update UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import './App.css';
 import Home from './Pages/Home/HomePage.jsx'
 import AppLayout from './Pages/Layout/AppLayout.jsx';
+import Login from './Pages/Login/LoginForm.jsx';
+import SignUp from './Pages/Login/SignUpForm.jsx';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
         <Route element ={<AppLayout/>}>
           <Route path='/' element={<Home/>}/>
         </Route>
