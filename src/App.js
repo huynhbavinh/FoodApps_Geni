06bbe9bce00820acb293205b72eef3fd2f5b69e1
Phi <110226283+Phitcp@users.noreply.github.com>
2023-09-04 15:04:03 +0700

    Update context Auth

--- src/App.js
+++ src/App.js
@@ -4,19 +4,36 @@ import Home from './Pages/Home/HomePage.jsx'
 import AppLayout from './Pages/Layout/AppLayout.jsx';
 import Login from './Pages/Login/LoginForm.jsx';
 import SignUp from './Pages/Login/SignUpForm.jsx';
-
+import ProtectedRoute from './Pages/Layout/ProtectedRoute.jsx'
+import UserProfile from './Pages/Users/UserProfile.jsx';
+import AuthProvider from './Context/AuthContext.jsx'
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-        <Route element ={<AppLayout/>}>
-          <Route path='/' element={<Home/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={
+            <ProtectedRoute>
+              <AppLayout/>
+            </ProtectedRoute>
+              }
+              > 
+            <Route index element={<Navigate replace to='Home'/>}/>
+            <Route path='/profile' element={<UserProfile/>}/>
+          </Route>
+        <Route index element={<Navigate replace to='Home'/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/menu' element={<SignUp/>}/>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
+/*
+vao page -> Home, Menu, , Contact register, Login (offline) - Cart, Add to Cart trong menu -> Bat phai login
+Login -> Profile, Cart
+*/
 export default App;
--- src/Pages/Login/LoginForm.jsx
+++ src/Pages/Login/LoginForm.jsx
@@ -1,63 +1,84 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useNavigate, useEffect } from 'react'
 import { Container, Row } from "react-bootstrap";
 import { Col } from 'reactstrap';
 import wallpaper from '../../Assets/images/SignUp-wallpaper.jpg'
+import { useAuth } from '../../Context/AuthContext.jsx';
+function Login () {
+    const navigate = useNavigate()
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('')
+    const {login, isAuthenticated} = useAuth();
 
-export default class Login extends Component {
-    render() {
-        return (
-            <Container fluid style={{
-                backgroundImage: `url(${wallpaper})`,
-                height: '100vh',
-            }}>
-                <Row>
-                    <Col className="d-flex flex-fill justify-content-center p-5 my-5 bg-light bg-opacity-75 text-dark rounded shadow-lg">
-                        <form style={{width: '25%'}}>
-                            <h3>Sign In</h3>
+    useEffect(() => {
+        if(isAuthenticated) {
+            navigate('/home')
+        }
+    },
+    [isAuthenticated, navigate]);
 
-                            <div className="mb-3">
-                                <label>Email address</label>
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    placeholder="Enter email"
-                                />
-                            </div>
-
-                            <div className="mb-3">
-                                <label>Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    placeholder="Enter password"
-                                />
-                            </div>
-
-                            <div className="mb-3">
-                                <div className="custom-control custom-checkbox">
-                                    <input
-                                        type="checkbox"
-                                        className="custom-control-input"
-                                        id="customCheck1"
-                                    />
-                                    <label className="custom-control-label" htmlFor="customCheck1"> 
-                                        Remember me
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div className="d-grid">
-                                <button type="submit" className="btn btn-primary">
-                                    Submit
-                                </button>
-                            </div>
-                            <p className="forgot-password text-right">
-                                Forgot <a href="#">password?</a>
-                            </p>
-                        </form>
-                    </Col>
-                </Row>
-            </Container>
-        )
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(userName && password) {
+            login(userName, password)
+        }
     }
+    return (
+        <Container fluid style={{
+            backgroundImage: `url(${wallpaper})`,
+            height: '100vh',
+        }}>
+            <Row>
+                <Col className="d-flex flex-fill justify-content-center p-5 my-5 bg-light bg-opacity-75 text-dark rounded shadow-lg">
+                    <form style={{width: '25%'}} onSubmit={handleSubmit}>
+                        <h3>Sign In</h3>
+
+                        <div className="mb-3">
+                            <label>User name</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter email"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="mb-3">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="mb-3">
+                            <div className="custom-control custom-checkbox">
+                                <input
+                                    type="checkbox"
+                                    className="custom-control-input"
+                                    id="customCheck1"
+                                />
+                                <label className="custom-control-label" htmlFor="customCheck1"> 
+                                    Remember me
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="d-grid">
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
+                        </div>
+                        <p className="forgot-password text-right">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </form>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
+export default Login
