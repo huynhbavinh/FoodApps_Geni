9a3c2a3c14e58a7b0ace211178a0ce54f6b8e70c
Geni <binh.pham.set18@eiu.edu.vn>
2023-09-10 16:53:18 +0700

    update

--- src/Pages/CheckOut/CheckOutList.jsx
+++ src/Pages/CheckOut/CheckOutList.jsx
@@ -1,50 +1,43 @@
-import "./Products.scss";
-import { DataGrid } from "@mui/x-data-grid";
-import { cartColumns } from "../../Components/structure/datatablesource.js"
-import { Link, useNavigate } from "react-router-dom";
+import "./list.scss"
+
+import Carts from "./CheckOut.jsx";
+import FormCheckOut from "./FormCheckOut.jsx";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useEffect, useState } from "react";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
 import { useAuth } from "../../Context/AuthContext.jsx";
-const Products = ({items}) => {
+
+const CheckOutList = () => {
+  const [totalMoney, setTotalMoney] = useState(0);
+  const [carts, setCart] = useState(0);
   const { user } = useAuth()
-  const [flag, setFlag] = useState(0);
 
-  const handleChangeQuantity = (id, quantity) => {
-    
-  };
-  const actionColumn = [
-    {
-      field: "action",
-      headerName: "Action",
-      width: 200,
-      renderCell: (params) => {
-        return (
-          <div className="cellAction">
-            <div className="viewButton"
-              onClick={() => handleChangeQuantity(params.row.food.id, 1)}>Add 1</div>
-            <div
-              className="deleteButton"
-              onClick={() => handleChangeQuantity(params.row.food.id, -1)}
-            >
-              Remove 1
-            </div>
-          </div>
-        );
-      },
-    },
-  ];
+  useEffect(() => {
+    if (user) {
+      let total = 0;
+      const fetch = async () => {
+        const carts = (await axios.get(`http://localhost:8080/api/cart/cart?idUser=${user.data.id}`, {
+          'headers': { 'Authorization': `Bearer ${user.data.accessToken}` }
+        })).data;
+        setCart(carts)
+        carts.forEach(i => {
+          total += parseInt(i.quantity) * parseInt(i.food.price);
+        })
+        setTotalMoney(total);
+      }
+      fetch();
+    }
+  }, [])
   return (
-    <div className="datatable">
-      <DataGrid
-        className="datagrid"
-        rows={items}
-        columns={cartColumns.concat(actionColumn)}
-        pageSize={6}
-        rowsPerPageOptions={[6]}
-        flag={flag}
-      />
-    </div>
-  );
-};
+    <Container fluid>
+      <Row>
+        <Col xs={6}><Carts items={carts} /></Col>
+        <Col><FormCheckOut /></Col>
+      </Row>
+    </Container>
+  )
+}
 
-export default Products;
+export default CheckOutList
